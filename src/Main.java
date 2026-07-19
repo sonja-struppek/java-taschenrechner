3f2dfd610fc6434661ff9cb549fd2d95d8a1e21b
SONJA STRUPPEK <sonja.struppek@web.de>
2026-07-19 14:32:37 +0200

    Implement addition feature

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Mein Java Projekt läuft schonmal!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Zahl 1: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Zahl 2: ");
+        double b = scanner.nextDouble();
+
+        double result = a + b;
+        System.out.println("Ergebnis: " + result);
     }
 }
